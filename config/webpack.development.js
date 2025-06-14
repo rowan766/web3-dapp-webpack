4de0a7e67cb41dd39f119a5e3b7935f6c0dcf7bb
张欢 <570436048@qq.com>
2025-06-14 21:56:26 +0800

    项目初始化

---
+++ config/webpack.development.js
@@ -0,0 +1,64 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve,join } = require('path');
+const FriendlyErrorsWebpackPlugin = require('@soda/friendly-errors-webpack-plugin');
+const notifier = require('node-notifier');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const port = 3003;
+module.exports = {
+ devServer: {
+    //单页的spa应用 使用起来
+    historyApiFallback: true,
+    static: {
+      directory: join(__dirname, '../dist'),
+    },
+    hot: true,
+    port,
+  },
+  output: {
+    publicPath: '/',
+    //如果是通过loader 编译的 放到scripts文件夹里 filename
+    filename: 'scripts/[name].bundle.js',
+    //如果是通过'asset/resource' 编译的
+    assetModuleFilename: 'images/[name].[ext]',
+  },
+ plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      favicon: './public/favicon.ico',
+      template: resolve(__dirname, '../src/index-dev.html'),
+    }),
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: ['You application is running here http://localhost:' + port],
+        notes: ['💊 构建信息请及时关注窗口右上角'],
+      },
+      // new WebpackBuildNotifierPlugin({
+      //   title: '💿 Solv Dvelopment Notification',
+      //   logo,
+      //   suppressSuccess: true,
+      // }),
+      onErrors: function (severity, errors) {
+        if (severity !== 'error') {
+          return;
+        }
+        const error = errors[0];
+        console.log(error);
+        notifier.notify({
+          title: '👒 Webpack Build Error',
+          message: severity + ': ' + error.name,
+          subtitle: error.file || '',
+          icon: join(__dirname, 'icon.png'),
+        });
+      },
+      clearConsole: true,
+    }),
+    new BundleAnalyzerPlugin(
+      {
+        openAnalyzer: false, // 阻止自动打开性能监控端口
+        analyzerMode: 'server', // 或者 'static', 'json', 'disabled'
+        analyzerPort: 8889, // 可以指定端口
+      }
+    )
+ ]
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const merge = require('webpack-merge');
+const argv = require('yargs-parser')(process.argv.slice(2));
+const { resolve } = require('path');
+const _mode = argv.mode||'development'
+const _mergeConfig = require(`./config/webpack.${_mode}.js`);
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const _modeflag = _mode === 'production' ? true : false;
+
+const { ThemedProgressPlugin } = require('themed-progress-plugin');
+
+
+const webpackBaseConfig={
+    entry:{
+        main: resolve('src/index.tsx'),
+    },
+    output:{
+        path: resolve(process.cwd(), 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /(node_modules)/,
+                use: {
+                // `.swcrc` can be used to configure swc
+                loader: 'swc-loader',
+                },
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // 'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'postcss-loader',
+                ],
+                // use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+        '@': resolve('src/'),
+        '@components': resolve('src/components'),
+        '@hooks': resolve('src/hooks'),
+        '@pages': resolve('src/pages'),
+        '@layouts': resolve('src/layouts'),
+        '@assets': resolve('src/assets'),
+        '@states': resolve('src/states'),
+        '@service': resolve('src/service'),
+        '@utils': resolve('src/utils'),
+        '@lib': resolve('src/lib'),
+        '@constants': resolve('src/constants'),
+        '@connections': resolve('src/connections'),
+        '@abis': resolve('src/abis'),
+        '@types': resolve('src/types'),
+        },
+        extensions: ['.js', '.ts', '.tsx', '.jsx', '.css'],
+        fallback: {
+        // stream: require.resolve('stream-browserify'),
+        },
+    },
+    plugins:[
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: _modeflag ? 'styles/[name].[contenthash:5].css' : 'styles/[name].css',
+            chunkFilename: _modeflag ? 'styles/[name].[contenthash:5].css' : 'styles/[name].css',
+            ignoreOrder: false,
+        }),
+        new ThemedProgressPlugin({
+            theme: 'halloween', // 可能的选项: 'default', 'halloween', 'christmas', 'spring', 'summer', 'custom'
+        }),
+    ],
+    stats: 'errors-only', // 只显示错误信息
+};
+module.exports = merge.default(webpackBaseConfig, _mergeConfig);
